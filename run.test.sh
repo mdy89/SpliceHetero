ab4653481c4a7755d9b294aee7241041e4096e8d
mdy89 <35953866+mdy89@users.noreply.github.com>
2019-01-07 21:56:33 +0900

    Update run.test.sh

--- run.test.sh
+++ run.test.sh
@@ -1,6 +1,6 @@
 ## A command for calculating sITH of samples in CANCER_SAMPLES.txt against the samples in NORMAL_SAMPLES.txt
 ## "-prn 3" indicates the program will use three processes in parallel
-## "-slb False" indicates the RNA-seq library of input is not strand-specific (if both libraries are mixed, specify False)
+## "-slb False" indicates the RNA-seq library of input is not strand-specific (if you don't know which, it is safe to choose False)
 ## The input format is BED. See CANCER_SAMPLES.txt and NORMAL_SAMPLES.txt for input format
 
 python SpliceHetero.py -csp CANCER_SAMPLES.txt -rsp NORMAL_SAMPLES.txt -odp TEST_OUT.Dir -prn 3 -slb False
